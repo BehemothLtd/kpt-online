98a7262686752da9be1012c88900603f7fd3abb5
bachdx <bachdx.hut@gmail.com>
2020-04-12 12:52:04 +0700

    add some fukcing ui

--- front/src/components/Cards.vue
+++ front/src/components/Cards.vue
@@ -1,7 +1,29 @@
 <template>
   <div>
-    <ul>
-      <li v-for="card in cards" :key="card.id">
+    <ul class="keep">
+      <li v-for="card in keeps" :key="card.id">
+        <div>
+          {{ card.content }}
+          <br />
+          {{ card.type }}
+          <br />
+          {{ card.user }}
+        </div>
+      </li>
+    </ul>
+    <ul class="problem">
+      <li v-for="card in problems" :key="card.id">
+        <div>
+          {{ card.content }}
+          <br />
+          {{ card.type }}
+          <br />
+          {{ card.user }}
+        </div>
+      </li>
+    </ul>
+    <ul class="try">
+      <li v-for="card in trys" :key="card.id">
         <div>
           {{ card.content }}
           <br />
@@ -21,9 +43,29 @@ export default {
       type: Array,
       required: true
     }
+  },
+  computed: {
+    keeps: function() {
+      return this.cards.filter(c => c.type == 1);
+    },
+    problems: function() {
+      return this.cards.filter(c => c.type == 2);
+    },
+    trys: function() {
+      return this.cards.filter(c => c.type == 3);
+    }
   }
 };
 </script>
 
 <style>
+.keep {
+  background-color: green;
+}
+.problem {
+  background-color: red;
+}
+.try {
+  background-color: yellow;
+}
 </style>
